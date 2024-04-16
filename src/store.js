15b54a410cd084aa2bc7a238f83f8fa8523062bc
mfadhili <mfadhili@github.com>
2024-04-16 04:34:04 +0300

    refactor: default export for reducers

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import {combineReducers, createStore} from "redux";
-import {createCustomer, customerReducer, updateName} from "./features/customers/customerSlice";
-import {accountReducer, deposit, payLoan, requestLoan, withdraw} from "./features/account/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+import accountReducer from "./features/account/accountSlice";
 
 
 const rootReducer = combineReducers({
@@ -8,25 +8,25 @@ const rootReducer = combineReducers({
     customer: customerReducer,
 })
 const store =createStore(rootReducer);
-
+export default store;
 /*ACTION CREATORS*/
 
 
 
-store.dispatch(deposit(5000));
-store.dispatch(withdraw(200));
-console.log(store.getState());
-
-store.dispatch(requestLoan(2000, "Buy car"));
-console.log(store.getState());
-
-store.dispatch(payLoan());
-console.log(store.getState());
-
-
-/*CUSTOMER DISPATCH*/
-store.dispatch(createCustomer('fullName fullName', '222222222'));
-console.log(store.getState());
-
-store.dispatch(updateName("Namefull"));
-console.log(store.getState());
+// store.dispatch(deposit(5000));
+// store.dispatch(withdraw(200));
+// console.log(store.getState());
+//
+// store.dispatch(requestLoan(2000, "Buy car"));
+// console.log(store.getState());
+//
+// store.dispatch(payLoan());
+// console.log(store.getState());
+//
+//
+// /*CUSTOMER DISPATCH*/
+// store.dispatch(createCustomer('fullName fullName', '222222222'));
+// console.log(store.getState());
+//
+// store.dispatch(updateName("Namefull"));
+// console.log(store.getState());
