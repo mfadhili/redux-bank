9d5f5ef98ebfaf31ae7dc3044997dad724147216
mfadhili <mfadhili@github.com>
2024-04-21 20:31:20 +0300

    fix

--- src/store.js
+++ src/store.js
@@ -2,13 +2,17 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/account/accountSlice";
 import {thunk} from "redux-thunk";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 
 const rootReducer = combineReducers({
     account: accountReducer,
     customer: customerReducer,
 })
-const store =createStore(rootReducer, applyMiddleware(thunk));
+const store =createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
 export default store;
 /*ACTION CREATORS*/
 
